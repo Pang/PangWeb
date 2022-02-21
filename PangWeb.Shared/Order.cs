357b295e25449ea9072889b86c33a637609b1d1b
Ryan <ryan.beckett@pastest.co.uk>
2022-02-21 15:33:08 +0000

    Shop Models for purchasing

---
+++ PangWeb.Shared/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace PangWeb.Shared
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string TotalOrderPrice { get; set; }
+        public DateTimeOffset OrderPlacedDt { get; set; }
+        public bool OrderComplete { get; set; }
+        public bool Cancelled { get; set; }
+
+        public User User { get; set; }
+        public List<ItemPurchase> ItemPurchase { get; set; }
+    }
+}
